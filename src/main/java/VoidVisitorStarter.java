4633c73a0622d37ab85ded522d85db869c4a849d
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2019-06-27 15:14:19 +0200

    Resolving types, detecting upcasting or not

--- src/main/java/VoidVisitorStarter.java
+++ src/main/java/VoidVisitorStarter.java
@@ -8,6 +8,13 @@ import com.github.javaparser.ast.visitor.VoidVisitor;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import com.github.javaparser.printer.DotPrinter;
 import com.github.javaparser.printer.YamlPrinter;
+import com.github.javaparser.resolution.types.ResolvedType;
+import com.github.javaparser.symbolsolver.javaparsermodel.JavaParserFacade;
+import com.github.javaparser.symbolsolver.model.resolution.TypeSolver;
+import com.github.javaparser.symbolsolver.resolution.SymbolSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -15,13 +22,19 @@ import java.io.PrintWriter;
 
 public class VoidVisitorStarter {
 
-    private static final String FILE_PATH;
+    private static final String FILE_PATH, SOURCE_PATH;
+    private static TypeSolver typeSolver;
+    private static SymbolSolver symbolSolver;
 
     static {
         FILE_PATH = "src/main/java/org/javaparser/samples/ReversePolishNotation.java";
+        SOURCE_PATH = "src/main/java/org/javaparser/samples/";
+        typeSolver = new CombinedTypeSolver(new ReflectionTypeSolver(), new JavaParserTypeSolver(SOURCE_PATH));
+        symbolSolver = new SymbolSolver(typeSolver);
     }
 
     public static void main(String[] args) throws Exception {
+
         CompilationUnit cu = StaticJavaParser.parse(new File(FILE_PATH));
 //        DotPrinter printer = new DotPrinter(true);
 //        try (FileWriter fileWriter = new FileWriter("ast.dot");
@@ -36,41 +49,30 @@ public class VoidVisitorStarter {
 
     private static class MyPrinter extends VoidVisitorAdapter<Void> {
 
-        //        @Override
-//        public void visit(VariableDeclarationExpr vd, Void arg) {
-//            super.visit(vd, arg);
-//            System.out.println("Declaration: " + vd.toString());
-//            for (VariableDeclarator theVD:vd.getVariables()
-//                 ) {
-//                System.out.println("  Variable: " + theVD);
-//                if (theVD.getInitializer().isPresent()) System.out.println("  isNew: " + theVD.getInitializer().get());
-//            }
-//        }
-//        @Override
-//        public void visit(ObjectCreationExpr objectCreationExpr, Void arg) {
-//            super.visit(objectCreationExpr, arg);
-//            System.out.println("New: " + objectCreationExpr.toString() + " Type: " + objectCreationExpr.getType());
-//            if (objectCreationExpr.isAssignExpr()) {
-//                System.out.println("   Assignment scope: " + objectCreationExpr.getScope());
-//            } else if (objectCreationExpr.isVariableDeclarationExpr()) {
-//                System.out.println("   used in declaration: " + objectCreationExpr.asVariableDeclarationExpr().getVariables());
-//            }
-//        }
-
         @Override
         public void visit(AssignExpr assignExpr, Void arg) {
             super.visit(assignExpr, arg);
             assignExpr.getValue().ifObjectCreationExpr(objectCreationExpr -> {
-                System.out.println("New (assignExpr): " + objectCreationExpr
-                        + " ==> Type " + objectCreationExpr.getType());
-                System.out.print("  assigned to ");
-                assignExpr.getTarget().ifFieldAccessExpr(fieldAccessExpr -> {
-                    System.out.println("fieldAccess: " + fieldAccessExpr.getName());
-                });
-                assignExpr.getTarget().ifNameExpr(nameExpr -> {
-                    System.out.println("nameExpression: " + nameExpr.getName());
-                });
-                System.out.println("  in scope:" + assignExpr.findAncestor(CompilationUnit.class).
+                ResolvedType targetType, expressionType;
+                expressionType = JavaParserFacade.get(typeSolver).getType(objectCreationExpr);
+                targetType = JavaParserFacade.get(typeSolver).getType(assignExpr.getTarget());
+
+                System.out.println("New (assignExpr): " + assignExpr);
+//                System.out.println("    Type from resolver: " + JavaParserFacade.get(typeSolver).getType(objectCreationExpr));
+//                System.out.print("  assigned to ");
+//                assignExpr.getTarget().ifFieldAccessExpr(fieldAccessExpr -> {
+//                    System.out.println("fieldAccess: " + fieldAccessExpr.getName());
+//                });
+//                assignExpr.getTarget().ifNameExpr(nameExpr -> {
+//                    System.out.println("nameExpression: " + nameExpr.getName());
+//                });
+//                System.out.println("    Type from resolver: " + JavaParserFacade.get(typeSolver).getType(assignExpr.getTarget()));
+                if (targetType.equals(expressionType)) {
+                    System.out.println("NO upcasting -- same type (" + targetType + ") on both sides of assignment.");
+                } else {
+                    System.out.println("Upcasting -- from (" + expressionType + ") to (" + targetType + ")");
+                }
+                System.out.println("  in class:" + assignExpr.findAncestor(CompilationUnit.class).
                         flatMap(CompilationUnit::getPrimaryTypeName));
             });
         }
@@ -81,15 +83,20 @@ public class VoidVisitorStarter {
             super.visit(variableDeclarator, arg);
             variableDeclarator.getInitializer().ifPresent(expression -> {
                 expression.ifObjectCreationExpr(objectCreationExpr -> {
-                    System.out.println("New (variableDeclarator): " + objectCreationExpr
-                            + " ==> Type " + objectCreationExpr.getType());
-                    System.out.println("  assigned to type " + variableDeclarator.getType());
-                    if (variableDeclarator.getType().equals(objectCreationExpr.getType())) {
-                        System.out.println("NO upcasting.");
+                    ResolvedType targetType, expressionType;
+                    expressionType = JavaParserFacade.get(typeSolver).getType(objectCreationExpr);
+                    targetType = JavaParserFacade.get(typeSolver).getType(variableDeclarator);
+
+                    System.out.println("New (variableDeclarator): " + variableDeclarator);
+//                    System.out.println("    Type from resolver: " + expressionType);
+//                    System.out.println("  assigned to type " + variableDeclarator.getType());
+//                    System.out.println("    Type from resolver: " + targetType);
+                    if (targetType.asReferenceType().getQualifiedName().equals(expressionType.asReferenceType().getQualifiedName())) {
+                        System.out.println("NO upcasting -- same type (" + targetType + ") on both sides of assignment.");
                     } else {
-                        System.out.println("Upcasting.");
+                        System.out.println("Upcasting -- from (" + expressionType + ") to (" + targetType + ")");
                     }
-                    System.out.println("  in scope:" + variableDeclarator.findAncestor(CompilationUnit.class).
+                    System.out.println("  in class:" + variableDeclarator.findAncestor(CompilationUnit.class).
                             flatMap(CompilationUnit::getPrimaryTypeName));
                 });
             });
@@ -97,12 +104,4 @@ public class VoidVisitorStarter {
 
     }
 
-    private static class AssignmentPrinter extends VoidVisitorAdapter<Void> {
-
-        @Override
-        public void visit(AssignExpr ae, Void arg) {
-            super.visit(ae, arg);
-            System.out.println("Assignment: " + ae.toString() + " has objectCreation: " + ae.getValue().isObjectCreationExpr());
-        }
-    }
 }
