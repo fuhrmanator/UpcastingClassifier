df1ae75b6dd8a4137c3c9674f7ecb4b8491a5838
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2019-07-05 17:05:14 +0200

    Visualizing a sample file (more concrete) with DOT

--- src/main/java/ASTVisualizer.java
+++ src/main/java/ASTVisualizer.java
@@ -8,13 +8,13 @@ import java.io.PrintWriter;
 
 public class ASTVisualizer {
 
-    private static final String FILE_PATH = "C:/path/to/java/file.java";
+    private static final String FILE_PATH = "src/main/java/ABC.java";
 
     // See https://javaparser.org/inspecting-an-ast/
     public static void main(String[] args) throws Exception {
         CompilationUnit cu = StaticJavaParser.parse(new File(FILE_PATH));
         DotPrinter printer = new DotPrinter(true);
-        try (FileWriter fileWriter = new FileWriter("ast.dot");
+        try (FileWriter fileWriter = new FileWriter("ABC_ast.dot");
              PrintWriter printWriter = new PrintWriter(fileWriter)) {
             printWriter.print(printer.output(cu));
         }
