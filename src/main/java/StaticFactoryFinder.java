a092178b4aebc6b5bc7b19695497c9b11f8ea624
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2019-07-10 17:56:06 +0200

    Refactoring

--- src/main/java/StaticFactoryFinder.java
+++ src/main/java/StaticFactoryFinder.java
@@ -5,9 +5,9 @@ import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.Modifier;
-import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.expr.AssignExpr;
+import com.github.javaparser.ast.expr.Expression;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.resolution.declarations.ResolvedMethodDeclaration;
 import com.github.javaparser.symbolsolver.JavaSymbolSolver;
@@ -23,7 +23,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class StaticFactoryFinder {
@@ -46,55 +46,52 @@ public class StaticFactoryFinder {
 
         List<ParseResult<CompilationUnit>> parseResults = sourceRoot.tryToParse("");
 
-
         // Now get all compilation unitsList
         List<CompilationUnit> allCus = parseResults.stream()
                 .filter(ParseResult::isSuccessful)
-                .map(r -> r.getResult().get())
+                .map(r -> {
+                    if (r.getResult().isPresent())
+                        return r.getResult().get();
+                    else return null;
+                })
                 .collect(Collectors.toList());
         List<String> staticFactoryMethods = new ArrayList<>();
-        for (CompilationUnit cu : allCus
-        ) {
+
+        /*
+         * Find the static factory methods
+         */
+        for (CompilationUnit cu : allCus) {
             staticFactoryMethods.addAll(cu.findAll(MethodDeclaration.class).stream()
-                    .filter(methodDeclaration -> methodDeclaration.getModifiers().contains(Modifier.staticModifier())
-                            && methodDeclaration.getName().getIdentifier().startsWith("create"))
-                    .map(methodDeclaration -> {
-                        ResolvedMethodDeclaration resolved = methodDeclaration.resolve();
-                        //System.out.println("Found " + resolved.getClassName() + "." + resolved.getName() + " in " + cu.getPrimaryType().get().resolve().getQualifiedName());
-                        return resolved.getClassName() + "." + resolved.getName();
-                    })
+                    .filter(methodDeclaration ->
+                            methodDeclaration.getModifiers().contains(Modifier.staticModifier())  // is STATIC
+                            && methodDeclaration.getName().getIdentifier().startsWith("create"))  // is a supposed Factory
+                    .map(methodDeclaration -> methodDeclaration.resolve().getQualifiedSignature())
                     .collect(Collectors.toList()));
         }
+        System.out.println("Found the following static factory methods:");
         staticFactoryMethods.forEach(System.out::println);
         System.out.println("-----------------");
+        // Two ways a factory method can be used:
+        // assignments, e.g., l = createList(...), or
+        // declarations, e.g. List l = createList(...)
         List<AssignExpr> invocationsOfStaticFactoriesInAssignments = new ArrayList<>();
         List<VariableDeclarator> invocationsOfStaticFactoriesInDeclarations = new ArrayList<>();
-        for (CompilationUnit cu : allCus
-        ) {
+        for (CompilationUnit cu : allCus) {
+            // assignment cases
             invocationsOfStaticFactoriesInAssignments.addAll(cu.findAll(AssignExpr.class).stream()
                     .filter(assignExpr -> {
                         if (assignExpr.getValue().isMethodCallExpr()) {
                             ResolvedMethodDeclaration resolved = assignExpr.getValue().asMethodCallExpr().resolve();
-                            System.out.println("  Found assignment with method invocation: " + resolved.getClassName() + "." + resolved.getName());
-                            //System.out.println("    in Class: " + cu.getPrimaryType().map(typeDeclaration -> typeDeclaration.resolve().getQualifiedName()));
-                            System.out.println("  2 in Class: " + /* cu.getType(0).resolve().getPackageName() + "|" + */ cu.getType(0).resolve().getQualifiedName());
-                            return staticFactoryMethods.contains(
-                                    resolved.getClassName()
-                                            + "." + resolved.getName()
-                            );
-                        }
-                        return false;
+                            return staticFactoryMethods.contains(resolved.getQualifiedSignature());
+                        } else return false;
                     })
                     .collect(Collectors.toList()));
+            // declaration cases
             invocationsOfStaticFactoriesInDeclarations.addAll(cu.findAll(VariableDeclarator.class).stream()
                     .filter(variableDeclarator -> {
                         if (variableDeclarator.getInitializer().isPresent() && variableDeclarator.getInitializer().get().isMethodCallExpr()) {
                             ResolvedMethodDeclaration resolved = variableDeclarator.getInitializer().get().asMethodCallExpr().resolve();
-                            System.out.println("  Found declaration with initialization via method invocation: " + resolved.getClassName() + "." + resolved.getName());
-                            System.out.println("  2 in Class: " + /* cu.getType(0).resolve().getPackageName() + "|" + */ cu.getType(0).resolve().getQualifiedName());
-                            return staticFactoryMethods.contains(
-                                    resolved.getClassName()
-                                            + "." + resolved.getName());
+                            return staticFactoryMethods.contains(resolved.getQualifiedSignature());
                         } else return false;
                     })
                     .collect(Collectors.toList()));
@@ -106,40 +103,25 @@ public class StaticFactoryFinder {
                 }
         );
         invocationsOfStaticFactoriesInDeclarations.forEach(variableDeclarator -> {
-                    System.out.print("Call to static factory in assignment: " +
-                            variableDeclarator.getInitializer().get());
-                    printContainingClassName(variableDeclarator);
-                }
+            Expression call = variableDeclarator.getInitializer().isPresent() ? variableDeclarator.getInitializer().get() : null;
+            System.out.print("Call to static factory in assignment: " +
+                call);
+            printContainingClassName(variableDeclarator);
+            }
         );
     }
 
-    public static List<CompilationUnit> getNodes(List<CompilationUnit> cus, Class nodeClass) {
-        List res = new LinkedList();
-        cus.forEach(cu -> res.addAll(cu.findAll(nodeClass)));
-        return res;
-    }
+//    public static List<CompilationUnit> getNodes(List<CompilationUnit> cus, Class nodeClass) {
+//        List<CompilationUnit> res = new LinkedList<>();
+//        cus.forEach(cu -> res.addAll(cu.findAll(nodeClass)));
+//        return res;
+//    }
 
     private static void printContainingClassName(@NotNull Node node) {
-        System.out.println("  in class: ");
-        Optional<CompilationUnit> ancestor = node.findAncestor(CompilationUnit.class);
-        //Optional<ClassOrInterfaceDeclaration> declaredType =
-        if (ancestor.isPresent()) {
-            CompilationUnit compilationUnit = ancestor.get();
-            if (compilationUnit.getPrimaryType().isPresent()) {
-                TypeDeclaration<?> typeDeclaration = compilationUnit.getPrimaryType().get();
-                    System.out.println("Package: " + typeDeclaration.resolve().getPackageName());
-                    System.out.println("Classname: " + typeDeclaration.resolve().getQualifiedName());
-            } else System.out.println("No primary type for ancestor for node " + node);
-        } else System.out.println("Could not find ancestor for node " + node);
-        
-//        System.out.println("  fully qualified name: " + getFullyQualifiedName(declaredType.get()));
-//        ancestor.ifPresent(compilationUnit -> compilationUnit.getPrimaryType().ifPresent(typeDeclaration -> {
-//            System.out.println(typeDeclaration.resolve().asReferenceType().getPackageName());
-//            System.out.println(typeDeclaration.getFullyQualifiedName());
-//        }));
-//        Optional<String> name = ancestor.flatMap(compilationUnit -> compilationUnit.getPrimaryTypeName());
-//        name.ifPresent(System.out::println);
+        System.out.print("  in class: ");
+        System.out.println(getFullyQualifiedName(Objects.requireNonNull(getClass(node))));
     }
+
     // See https://stackoverflow.com/a/55722326/1168342
     private static String getFullyQualifiedName(ClassOrInterfaceType e) {
         String name = "";
